cc7dfa0b3e91620f3ba218f195a87f89b71c7180
chlwlgns524 <chlwlgns524@naver.com>
2022-06-04 10:31:23 +0900

    프로젝트 초기화

---
+++ src/test/java/com/example/bitcoinproject/BitcoinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitcoinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
