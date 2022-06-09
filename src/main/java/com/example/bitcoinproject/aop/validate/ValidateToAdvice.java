5d1b8a0d51f753fcfa4aaeed7bd000c566893645
chlwlgns524 <chlwlgns524@naver.com>
2022-06-09 16:31:44 +0900

    Validate aop 추가

---
+++ src/main/java/com/example/bitcoinproject/aop/validate/ValidateToAdvice.java
@@ -0,0 +1,30 @@
+package com.example.bitcoinproject.aop.validate;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@Component
+@Aspect
+public class ValidateToAdvice {
+
+    @After(value = "@annotation(com.example.bitcoinproject.aop.validate.ValidateTo)")
+    public void isValidTo(JoinPoint joinPoint) {
+        boolean valid = isValidTime((LocalDateTime) Arrays.stream(joinPoint.getArgs())
+                .filter(o -> o instanceof LocalDateTime)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("to should exists!")));
+
+        if (!valid)
+            throw new IllegalArgumentException("Invalid to!");
+    }
+
+    private boolean isValidTime(LocalDateTime to) {
+        return to.compareTo(LocalDateTime.now()) < 0;
+    }
+
+}
